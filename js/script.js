c8f70906fb8bc0963bac4e952015e7ccf57d6db1
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-10-10 17:12:40 +0200

    Create style of the page

--- js/script.js
+++ js/script.js
@@ -1,46 +1,39 @@
-// Creao una variabile result per "immagazzinare" i valori del ciclo
-let result = '';
-const div = document.querySelector('div.container-fizz-buzz');
+const div = document.querySelector('div.container-fizzbuzz');
 
-// Creao il ciclo for per stampare numeri da 1 a 100
+
+// Creo il ciclo for per stampare numeri da 1 a 100
 for(let i = 1; i < 101; i++) {
+    // Creo l'elemento div ed lo assegno alla variabile square
+    let square = document.createElement('div');
 
     // Prima condizione per i numeri che sono sia multipli di 3 sia di 5 mi stampi "FizzBuzz"
     if(i % 3 == 0 && i % 5 == 0) {
-        result = 'FizzBuzz';
-        const fizzbuzz = document.createElement('div');
-        fizzbuzz.className = `box--${i}`;
-        fizzbuzz.append(`${result} ${i}`);
-        div.append(fizzbuzz);
-        console.log("FizzBuzz");
+        square.className = `div-fizzbuzz`;
+        square.append(`FizzBuzz`);
+
+        // console.log("FizzBuzz");
 
     // Seconda condizione per i numeri che sono multipli di 3 mi stampi "Fizz"    
     } else if (i % 3 == 0) {
-        result = 'Fizz';
-        const fizz = document.createElement('div');
-        fizz.className = `box--${i}`;
-        fizz.append(`${result} ${i}`);
-        div.append(fizz);
-        console.log("Fizz");
+        square.className = `div-fizz`;
+        square.append(`Fizz`);
+
+        // console.log("Fizz");
 
     // Terza condizione per i numeri che sono multipli di 5 mi stampi "Buzz"        
     } else if (i % 5 == 0) {
-        result = 'Buzz';
-        const buzz = document.createElement('div');
-        buzz.className = `box--${i}`;
-        buzz.append(`${result} ${i}`);
-        div.append(buzz);
-        console.log("Buzz");
+        square.className = `div-buzz`;
+        square.append(`Buzz`);
+
+        // console.log("Buzz");
 
     // Else per i restanti numeri        
     } else {
-        result = i;
-        const element = document.createElement('div');
-        element.className = `box--${i}`;
-        element.append(`${result}`);
-        div.append(element);
-        console.log(i);
+        square.className = `div-element`;
+        square.append(i);
+
+        // console.log(i);
     }
-    
+    div.append(square);
     
 }
